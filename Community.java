78d93935157d03494c8fe61b006229a2dbf1e99e
KingLem <king.lem@gmail.com>
2013-06-25 14:02:46 -0600

    Created Community class.

---
+++ Community.java
@@ -0,0 +1,16 @@
+package kinglem.Multitudes
+
+class Community extends Village{
+	short id;
+
+	//@todo: create constructor
+
+	/**@todo: assign initial families to comunity
+	*
+	*	Parameters:  List<Family>
+	*	Wat do: Add Families to this.relationships
+	*			change passed-in families' ids to list this community as their community
+	*
+	*/
+	
+}
